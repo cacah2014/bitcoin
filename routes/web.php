d6d073872a17863badaeeb05bd888b0f8e424cea
Cacah2014 <hosseini_sasan@yahoo.com>
2023-06-07 18:31:32 +0330

    new add

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/BITCOIN', function () {
+    return view('BITCOIN');
+    //return 'BITCOIN' ;
+});
+
